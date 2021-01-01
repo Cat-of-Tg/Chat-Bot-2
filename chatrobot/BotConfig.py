43ba005ad0060f3499b003ea6fa749c7d46e6990
スタークギャング <StarkGangZ@gmail.com>
2021-01-01 17:53:34 +0530

    Added Join To Message And Some Fixes ;)

--- chatrobot/BotConfig.py
+++ chatrobot/BotConfig.py
@@ -8,4 +8,7 @@ class Config(object):
     COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", "^/")
     DUMB_CHAT = int(os.environ.get("DUMB_CHAT", False))
     CUSTOM_START = os.environ.get("CUSTOM_START", None)
+    JMT_ENABLE = os.environ.get("JMT_ENABLE", False)
+    JMTC_ID = int(os.environ.get("JMTC_ID", False))
+    JMTC_LINK = os.environ.get("JMT_ENABLE", "t.me/PutLinkHereNIbbaElseHowPeopleWillKnow")
     CUSTOM_IMG = os.environ.get("CUSTOM_IMG", "https://static.botsrv.com/website/img/quriobot_favicon.1727b193.png")
