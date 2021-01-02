a041d51698e68b16544f9b46b5a2976ff136d7f0
スタークギャング <StarkGangZ@gmail.com>
2021-01-02 15:46:16 +0530

    Fix

--- chatrobot/plugins/help_me.py
+++ chatrobot/plugins/help_me.py
@@ -49,11 +49,7 @@ async def sedlyfsir(event):
         elif not already_added(event.sender_id):
             add_usersid_in_db(event.sender_id)
             await chatbot.send_message(Config.DUMB_CHAT, f"NEW USER ! \nUser ID : `{event.chat_id}`")
-        await chatbot.send_file(event.chat_id, file=Config.CUSTOM_IMG, caption=text_me, buttons = [
-             [custom.Button.inline("I Want My Own Bot 🔥", data="mewant")],
-             [custom.Button.inline("Close 🔐", data="close ")],
-              ]
-             )
+        await chatbot.send_file(event.chat_id, file=Config.CUSTOM_IMG, caption=text_me)
     
 
     
